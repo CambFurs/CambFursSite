9cb0f7f5a9235a32489f2bb3e8065fa46df0d8b1
Dan Robertson <akrobertson@mail.com>
2024-02-19 00:40:16 +0000

    changed social colours to black and added code to apply light/dark switching to social.img

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -7,7 +7,7 @@ const Socials = () =>  (
     {socials.map((social) => (
       <div key={social.id} className={`flex-1 justify-start items-center flex-row m-3`}>
         <a href={social.link} target='_blank' rel='noreferrer' className='container justify-center'>
-          <img src={social.image} alt={social.name} className='w-[40px] h-[40px] object-contain mx-1'/>
+            <img src={social.image} alt={social.name} className={`w-[40px] h-[40px] object-contain mx-1 ${social.name !== 'Barq' ? 'dark:invert' : ''}`}/>
           <p className='font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] dark:text-white text-black'>{social.name}</p>
         </a>
       </div>
