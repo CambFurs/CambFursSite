3b72eadbb4837bdfa4cecfe65d47da7e8a1b6a49
Snipsel <git@snipsel.net>
2025-07-24 18:01:32 +0100

    updated telegram link

--- src/components/JoinButton.tsx
+++ src/components/JoinButton.tsx
@@ -10,7 +10,7 @@ const JoinButton = (props: JoinButtonProps) => {
     return (
         <div className={`${styles.flexCenter} mx-5 w-[140px] h-[140px] rounded-full dark:bg-blue-gradient bg_orange_button p-[3px] cursor-pointer hover:animate-pulse`}>
             <div className={` ${styles.flexCenter} flex-col dark:bg-primary bg-primary-light w-[100%] h-[100%] rounded-full`}>
-                <a href="https://t.me/+378EgIdd3FphMjM0" rel='external' target='_blank'>    
+                <a href="https://t.me/+pNUPO05aFcgzMDRk" rel='external' target='_blank'>
                     <div className={` ${styles.flexStart} flex-row`}>
                         <p className='font-poppins font-medium text-[18px] leading-[23px]'>
                             <span className='dark:text-gradient text-orange-gradient'>Join </span>    
